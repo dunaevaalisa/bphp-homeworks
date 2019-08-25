6985b81dd8051564c318c5c73c5ec84d1a1879bc
dunaevaalisa <48627604+dunaevaalisa@users.noreply.github.com>
2019-08-25 12:18:17 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<?php
+$variable = 3.14;
+if( is_bool($variable)) {
+$type = ‘boolean’;
+} elseif (is_float($variable)) {
+$type = ‘float’;
+} elseif (is_int(’$variable’)) {
+$type = ‘int’;
+} elseif (is_string($variable)) {
+$type = ’string’;
+} elseif(is_null($variable)) {
+$type = ‘null’;
+} else {
+$type = ‘other’;
+}
+?>
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,6 +33,7 @@
             <li>Занятие 1.1. Знакомство с PHP
                 <ul>
                     <li><a href="1.1-intro-and-branching/1.1.0">Задание 1.1.0</a></li>
+                    <p><?="$variable is $type»?></p>
                     <li><a href="1.1-intro-and-branching/1.1.1">Задание 1.1.1</a></li>
                     <li><a href="1.1-intro-and-branching/1.1.2">Задание 1.1.2</a></li>
                 </ul>
