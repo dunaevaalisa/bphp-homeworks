b7f8f96d1a621caeaa6c7df46348882fdd977d8c
dunaevaalisa <48627604+dunaevaalisa@users.noreply.github.com>
2019-08-26 14:17:55 +0300

    Update index.php

--- 1.1-intro-and-branching/1.1.1/index.php
+++ 1.1-intro-and-branching/1.1.1/index.php
@@ -1,16 +1,31 @@
 <?php
-$variable = 3.14;
-if( is_bool($variable)) {
-$type = ‘boolean’;
+$variable = [];
+if (is_bool($variable)) {
+    $variable = 'true';
+    $type = 'boolean';
 } elseif (is_float($variable)) {
-$type = ‘float’;
-} elseif (is_int(’$variable’)) {
-$type = ‘int’;
+    $type = 'float';
+} elseif (is_int($variable)) {
+    $type = 'integer';
 } elseif (is_string($variable)) {
-$type = ’string’;
-} elseif(is_null($variable)) {
-$type = ‘null’;
+    $type = 'string';
+} elseif (is_null($variable)) {
+    $type = 'null';
 } else {
-$type = ‘other’;
-}
+    $type = 'other';
+};
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>bPHP - 1.1.1</title>
+</head>
+<body>
+    <p>
+        <?="$variable is $type";?>
+    </p>
+</body>
+</html>
