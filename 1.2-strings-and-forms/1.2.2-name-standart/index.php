f55d6b2af5128a610e2cddc244af89131c0dcec6
dunaevaalisa <48627604+dunaevaalisa@users.noreply.github.com>
2019-08-27 17:02:45 +0300

    Update index.php

--- 1.2-strings-and-forms/1.2.2-name-standart/index.php
+++ 1.2-strings-and-forms/1.2.2-name-standart/index.php
@@ -1,11 +1,11 @@
 <?php
-$name = mb_convert_case(trim($_POST['name']), MB_CASE_TITLE, "UTF-8");
-$surname = mb_convert_case(trim($_POST['surname']), MB_CASE_TITLE, "UTF-8");
+$firstName = mb_convert_case(trim($_POST['firstName']), MB_CASE_TITLE, "UTF-8");
+$middleName = mb_convert_case(trim($_POST['middleName']), MB_CASE_TITLE, "UTF-8");
 $lastName = mb_convert_case(trim($_POST['lastName']), MB_CASE_TITLE, "UTF-8");
 
-$fullname = $name. ‘ ‘ . $surname . ‘ ‘ . $initials;
-$fio = substr($name,0, 1). ‘ ’ . substr($surname, 0,1) . ‘ ‘ .substr($initials);
-$surnameAndInitials = substr($surname,0,1)) . ‘ ‘ . substr($name,0, 1)). '.' .substr($initials));
+$fullName = $lastName . ' ' . $firstName . ' ' . $middleName;
+$fio = mb_substr($lastName, 0, 1) . mb_substr($firstName, 0, 1) . mb_substr($middleName, 0, 1);
+$surnameAndInitials = $lastName . ' ' . mb_substr($firstName, 0, 1) . '.' . mb_substr($middleName, 0, 1) . '.';
 
 ?>
 
