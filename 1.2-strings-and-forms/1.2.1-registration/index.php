d3987e2a04f102c167942f03a2357fe29e535394
dunaevaalisa <48627604+dunaevaalisa@users.noreply.github.com>
2019-08-27 18:25:31 +0300

    Update index.php

--- 1.2-strings-and-forms/1.2.1-registration/index.php
+++ 1.2-strings-and-forms/1.2.1-registration/index.php
@@ -9,7 +9,7 @@ if (preg_match('/\W/', $data['login'])) {
     echo 'Длина пароля должна быть минимум 8 символов';
 } elseif (preg_match('/^\w+@[a-zA-Z]+\.[a-zA-Z]+$/', trim($data['email'])) === 0) {
     echo 'Неверный формат почтового адреса';
-} elseif (!$data['firstName'] || !$data['lastName'] || !$data['middleName']) {
+} elseif (('/\W/', !$data['firstName'] || ('/\W/', !$data['lastName'] || ('/\W/', !$data['middleName']) {
     echo 'ФИО обязательно к заполнению';
 } elseif ($codeWord !== $data['code']) {
     echo 'Неверно введено кодовое слово';
