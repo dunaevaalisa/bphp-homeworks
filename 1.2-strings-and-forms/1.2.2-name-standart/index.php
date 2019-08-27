2da59d84b0a4f67127537635504b80b39f20cf75
dunaevaalisa <48627604+dunaevaalisa@users.noreply.github.com>
2019-08-27 12:46:11 +0300

    Update index.php

--- 1.2-strings-and-forms/1.2.2-name-standart/index.php
+++ 1.2-strings-and-forms/1.2.2-name-standart/index.php
@@ -1,1 +1,7 @@
+$name = mb_convert_case(trim($_POST['name']), MB_CASE_TITLE, "UTF-8");
+$surname = mb_convert_case(trim($_POST['surname']), MB_CASE_TITLE, "UTF-8");
+$lastName = mb_convert_case(trim($_POST['lastName']), MB_CASE_TITLE, "UTF-8");
 
+$fullname = $name. ‘ ‘ . $surname . ‘ ‘ . $initials;
+$fio = substr($name,0, 1). ‘ ’ . substr($surname, 0,1) . ‘ ‘ .substr($initials);
+$surnameAndInitials = substr($surname,0,1)) . ‘ ‘ . substr($name,0, 1)). '.' .substr($initials));
