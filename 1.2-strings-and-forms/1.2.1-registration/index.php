523e55a62d71ed7b34d71fbce27ed6655c3aaef0
dunaevaalisa <48627604+dunaevaalisa@users.noreply.github.com>
2019-08-27 12:49:42 +0300

    Update index.php

--- 1.2-strings-and-forms/1.2.1-registration/index.php
+++ 1.2-strings-and-forms/1.2.1-registration/index.php
@@ -1,1 +1,19 @@
+<?php
+$data = $_POST;
+$codeWord = 'nd82jaake';
+if (preg_match('/\W/', $data['login'])) {
+    echo 'Поле логин не должно содержать символы @/*?,;:.';
+} elseif (!$data['login']) {
+    echo 'Поле логин обязательно к заполнению';
+} elseif (strlen($data['password']) < 8) {
+    echo 'Длина пароля должна быть минимум 8 символов';
+} elseif (preg_match('/^\w+@[a-zA-Z]+\.[a-zA-Z]+$/', trim($data['email'])) === 0) {
+    echo 'Неверный формат почтового адреса';
+} elseif (!$data['firstName'] || !$data['lastName'] || !$data['middleName']) {
+    echo 'ФИО обязательно к заполнению';
+} elseif ($codeWord !== $data['code']) {
+    echo 'Неверно введено кодовое слово';
+} else {
+    echo 'У вас все хорошо!:)';
+};
 
