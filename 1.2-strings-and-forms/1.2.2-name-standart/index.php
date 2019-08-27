59e10bf71ed100050fcdf1289966d474a9c837d3
dunaevaalisa <48627604+dunaevaalisa@users.noreply.github.com>
2019-08-27 12:47:54 +0300

    Update index.php

--- 1.2-strings-and-forms/1.2.2-name-standart/index.php
+++ 1.2-strings-and-forms/1.2.2-name-standart/index.php
@@ -1,3 +1,4 @@
+<?php
 $name = mb_convert_case(trim($_POST['name']), MB_CASE_TITLE, "UTF-8");
 $surname = mb_convert_case(trim($_POST['surname']), MB_CASE_TITLE, "UTF-8");
 $lastName = mb_convert_case(trim($_POST['lastName']), MB_CASE_TITLE, "UTF-8");
@@ -5,3 +6,20 @@ $lastName = mb_convert_case(trim($_POST['lastName']), MB_CASE_TITLE, "UTF-8");
 $fullname = $name. ‘ ‘ . $surname . ‘ ‘ . $initials;
 $fio = substr($name,0, 1). ‘ ’ . substr($surname, 0,1) . ‘ ‘ .substr($initials);
 $surnameAndInitials = substr($surname,0,1)) . ‘ ‘ . substr($name,0, 1)). '.' .substr($initials));
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>index.php</title>
+</head>
+<body>
+    <?="<p>Полное имя: '$fullname'</p>";?>
+    <?="<p>Фамилия и инициалы: '$surnameAndInitials'</p>";?>
+    <?="<p>Аббревиатура: '$fio'</p>";?>
+</body>
+</html>
