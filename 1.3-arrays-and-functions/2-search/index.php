8f00da2adffb0a8e78a9b8f289f90c808681d996
dunaevaalisa <48627604+dunaevaalisa@users.noreply.github.com>
2019-08-30 18:44:08 +0300

    Update index.php

--- 1.3-arrays-and-functions/2-search/index.php
+++ 1.3-arrays-and-functions/2-search/index.php
@@ -1,1 +1,48 @@
+<?php
 
+$map = [
+    [true,false,false,false,false],
+    [false,true,true,true,false],
+    [false,false,true,false,false],
+    [false,true,false,false,false],
+    [false,false,false,true,false],
+    [true,false,true,false,false],
+];
+$n = 4;
+$result = findSeats($map, $n);
+logResult($result);
+function findSeats($map, $n) 
+{
+    $seats = [];
+    if (!isset($map)) return;
+    foreach ($map as $key => $value) {
+        $seatNum = 0;
+        if ($n > count($value) || !in_array(false, $value)) return;
+        $stop = (count($value) - $n) + 1;
+        foreach ($value as $seat) {
+            
+            $seatNum++;
+            if (count($seats) < $n) {
+                
+                $stop--;
+                if (!$seat) {
+                    $seats[] = $seatNum;
+                    if (count($seats) === $n) return [++$key, $seats];
+                } else {
+                    $seats = [];
+                    if ($stop <= 0) break;
+                };
+            } else {
+                return [++$key, $seats];
+            };
+        };
+    };
+};
+function logResult($res)
+{
+    if ($res) {
+        echo "Поздравляем, Ваши места ".implode(",",$res[1])." в $res[0] ряду!";
+    } else {
+        echo 'Извините, места рядом не нашлись:(';
+    };
+};
