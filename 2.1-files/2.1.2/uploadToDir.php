5a9bcf6b1bec7801ee71f8d30ef0f283fccebbe1
dunaevaalisa <48627604+dunaevaalisa@users.noreply.github.com>
2019-09-01 13:15:19 +0300

    Update uploadToDir.php

--- 2.1-files/2.1.2/uploadToDir.php
+++ 2.1-files/2.1.2/uploadToDir.php
@@ -17,10 +17,6 @@ function uploadFileToDir($uploadsDir='', $fileName='')
         $tmpName = $_FILES[$fileName]['tmp_name'];
         
         if (is_uploaded_file($tmpName)) {
-        // Использование этой функции целесообразно для удостоверения,
-        // что злонамеренный пользователь не пытается обмануть скрипт так,
-        // чтобы он работал с файлами, с которыми работать не должен - к примеру,
-        // /etc/passwd.
             $pathParts = pathinfo($_FILES[$fileName]['name']);
             
             if (in_array($pathParts['extension'], $extType)) {
