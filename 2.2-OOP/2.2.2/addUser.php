f172fe42bff50883857f79ad86627c36a007c362
dunaevaalisa <48627604+dunaevaalisa@users.noreply.github.com>
2019-09-01 20:11:16 +0300

    Delete addUser.php

--- 2.2-OOP/2.2.2/addUser.php
+++
@@ -1,9 +0,0 @@
-<?php
-include '../autoload.php';
-include '../config/SystemConfig.php';
-$newUser = new User;
-$formData = $_POST;
-$newUser->addUserFromForm($formData);
-$newUser->commit();
-header('HTTP/1.1 200 OK'); 
-header('Location: http://' . $_SERVER['HTTP_HOST'] . '/2.2-OOP/2.2.2');
